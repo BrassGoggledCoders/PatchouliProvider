4e3e0e1f9ffb6ab10b33505530295be87b0251b7
Alan19 <Alan19@users.noreply.github.com>
2020-06-22 12:30:23 -0400

    Wrap text in formatting codes

--- src/main/java/xyz/brassgoggledcoders/patchouliprovider/FormatHelper.java
+++ src/main/java/xyz/brassgoggledcoders/patchouliprovider/FormatHelper.java
@@ -2,81 +2,84 @@ package xyz.brassgoggledcoders.patchouliprovider;
 
 import net.minecraft.client.settings.KeyBinding;
 
+import static java.lang.String.valueOf;
+
 public class FormatHelper {
-    public static String clearFormatting(){
+    public static String clearFormatting() {
         return wrapTextInFormattingCode("");
     }
 
-    public static String lineBreak(){
+    public static String lineBreak() {
         return wrapTextInFormattingCode("br");
     }
 
-    public static String newParagraph(){
+    public static String newParagraph() {
         return wrapTextInFormattingCode("br2");
     }
 
-    public static String bulletedList(int nestedLevel){
-        return wrapTextInFormattingCode(String.valueOf(nestedLevel));
+    public static String bulletedList(int nestedLevel, String text) {
+        return wrapTextInFormattingCode(String.valueOf(nestedLevel)) + text + clearFormatting();
     }
 
-    public static String hexCodeColor(int color){
-        return wrapTextInFormattingCode("#" + Integer.toHexString(color));
+    public static String hexCodeColor(int color, String text) {
+        return wrapTextInFormattingCode("#" + Integer.toHexString(color)) + text + clearFormatting();
     }
 
-    public static String colorCode(char lowercaseHexValue){
-        return wrapTextInFormattingCode(String.valueOf(lowercaseHexValue));
+    public static String colorCode(char lowercaseHexValue, String text) {
+        return wrapTextInFormattingCode(String.valueOf(lowercaseHexValue)) + text + clearFormatting();
     }
 
-    public static String formatCode(char formatCode){
-        return wrapTextInFormattingCode(String.valueOf(formatCode));
+    public static String formatCode(char formatCode, String text) {
+        return wrapTextInFormattingCode(String.valueOf(formatCode))  + text + clearFormatting();
     }
 
-    public static String internalLink(String entryID){
-        return wrapTextInFormattingCode("l:" + entryID);
+    public static String internalLink(String entryID, String text) {
+        return wrapTextInFormattingCode("l:" + entryID) + text + endLink();
     }
 
-    public static String internalLink(String entryID, String anchorName){
-        return wrapTextInFormattingCode("l:" + entryID + "#" + anchorName);
+    public static String internalLink(String entryID, String anchorName, String text) {
+        return wrapTextInFormattingCode("l:" + entryID + "#" + anchorName) + text + endLink();
     }
 
-    public static String externalLink(String link){
-        return wrapTextInFormattingCode("l" + link);
+    public static String externalLink(String link, String text) {
+        return wrapTextInFormattingCode("l" + link) + text + endLink();
     }
 
-    public static String endLink(){
+    public static String endLink() {
         return wrapTextInFormattingCode("/l");
     }
 
-    public static String playerName(){
+    public static String playerName() {
         return wrapTextInFormattingCode("playername");
     }
 
-    public static String keyValueOfKeybind(KeyBinding keyBinding){
+    public static String keyValueOfKeybind(KeyBinding keyBinding) {
         return wrapTextInFormattingCode("k:" + keyBinding.getKeyDescription());
     }
 
-    public static String tooltip(String tooltip){
-        return wrapTextInFormattingCode("t:" + tooltip);
+    public static String tooltip(String tooltip, String text) {
+        return wrapTextInFormattingCode("t:" + tooltip) + text + endTooltip();
     }
 
-    public static String endTooltip(){
+    public static String endTooltip() {
         return wrapTextInFormattingCode("/t");
     }
 
     /**
      * Sends the command to the server when text with formatting is clicked
+     *
      * @param command The command to be sent to the server, without the "/" at the beginning
      * @return The text formatting code for sending a command to the server
      */
-    public static String sendCommandToServer(String command){
-        return wrapTextInFormattingCode("c:/" + command);
+    public static String sendCommandToServer(String command, String text) {
+        return wrapTextInFormattingCode("c:/" + command) + text + endCommand();
     }
 
-    public static String endCommand(){
+    public static String endCommand() {
         return wrapTextInFormattingCode("/c");
     }
 
-    private static String wrapTextInFormattingCode(String text){
+    private static String wrapTextInFormattingCode(String text) {
         return String.format("$(%s)", text);
     }
 }
