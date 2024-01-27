27d8636ee2cb8b47f8b44bcaa1cd2ee9a7676b20
Mrbysco <Mrbysco@users.noreply.github.com>
2024-01-27 19:17:34 +0100

    Update to 1.20.4

--- src/main/java/xyz/brassgoggledcoders/patchouliprovider/PatchouliProviderMod.java
+++ src/main/java/xyz/brassgoggledcoders/patchouliprovider/PatchouliProviderMod.java
@@ -1,6 +1,6 @@
 package xyz.brassgoggledcoders.patchouliprovider;
 
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.fml.common.Mod;
 
 @Mod("patchouli_provider")
 public class PatchouliProviderMod {
