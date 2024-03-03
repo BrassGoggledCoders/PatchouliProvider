e817b6f08073a17aaefa4e79ac4a24873dacbc30
Mrbysco <Mrbysco@users.noreply.github.com>
2024-03-04 00:05:59 +0100

    Link pages support the same attributes text pages do
    See https://vazkiimods.github.io/Patchouli/docs/patchouli-basics/page-types#link-pages-patchoulilink

--- src/main/java/xyz/brassgoggledcoders/patchouliprovider/page/LinkPageBuilder.java
+++ src/main/java/xyz/brassgoggledcoders/patchouliprovider/page/LinkPageBuilder.java
@@ -7,6 +7,9 @@ import xyz.brassgoggledcoders.patchouliprovider.EntryBuilder;
 public class LinkPageBuilder extends AbstractPageBuilder<LinkPageBuilder> {
     private final String url;
     private final String linkText;
+    // Link page support the same attributes as Text pages, see: https://vazkiimods.github.io/Patchouli/docs/patchouli-basics/page-types#link-pages-patchoulilink
+    private String text;
+    private String title;
 
     public LinkPageBuilder(String url, String linkText, EntryBuilder entryBuilder) {
         super("patchouli:link", entryBuilder);
@@ -14,9 +17,25 @@ public class LinkPageBuilder extends AbstractPageBuilder<LinkPageBuilder> {
         this.linkText = linkText;
     }
 
+    public LinkPageBuilder setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public LinkPageBuilder setText(String text) {
+        this.text = text;
+        return this;
+    }
+
     @Override
     protected void serialize(JsonObject json) {
         json.addProperty("url", url);
         json.addProperty("link_text", linkText);
+        if (title != null) {
+            json.addProperty("text", title);
+        }
+        if (title != null) {
+            json.addProperty("title", title);
+        }
     }
 }
